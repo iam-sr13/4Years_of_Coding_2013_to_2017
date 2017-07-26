5bade8b6f4035c73e689abbc34b3b68d8f3e72d0
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2017-07-26 16:29:09 +0530

    Silly bugfix - date_ops.cpp!
    Wtf! I had optimised and debugged the code for leap year and beyond and I found this silly bug now. Instead of july I had made june as a month of 31 days. lol! Silly me!

--- B.Tech.CSE/SY-Sem4/day9(Test)/Test(15CSE1036)/Test(final)/date_ops.cpp
+++ B.Tech.CSE/SY-Sem4/day9(Test)/Test(15CSE1036)/Test(final)/date_ops.cpp
@@ -19,7 +19,7 @@ class date
 	public:
 	int mdays(int m, int y=1)
 	{
-		if(m==0||m==1||m==3||m==5||m==6||m==8||m==10||m==12) return 31;			
+		if(m==0||m==1||m==3||m==5||m==7||m==8||m==10||m==12) return 31;			
 		else if(m!=2) return 30;
 		else if(y%4==0) return 29;
 		else return 28;
